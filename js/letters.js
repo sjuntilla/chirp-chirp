9055c01b418305d11cb1db07982fb47093865788
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-16 20:39:02 -1000

    Completes exercise.

--- js/letters.js
+++ js/letters.js
@@ -6,20 +6,51 @@ cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
 var counter = {
-  "a" : 0,  "b" : 0,  "c" : 0,  "d" : 0,
-  "e" : 0,  "f" : 0,  "g" : 0,  "h" : 0,
-  "i" : 0,  "j" : 0,  "k" : 0,  "l" : 0,
-  "m" : 0,  "n" : 0,  "o" : 0,  "p" : 0,
-  "q" : 0,  "r" : 0,  "s" : 0,  "t" : 0,
-  "u" : 0,  "v" : 0,  "w" : 0,  "x" : 0,
-  "y" : 0,  "z" : 0
+  "a": 0,
+  "b": 0,
+  "c": 0,
+  "d": 0,
+  "e": 0,
+  "f": 0,
+  "g": 0,
+  "h": 0,
+  "i": 0,
+  "j": 0,
+  "k": 0,
+  "l": 0,
+  "m": 0,
+  "n": 0,
+  "o": 0,
+  "p": 0,
+  "q": 0,
+  "r": 0,
+  "s": 0,
+  "t": 0,
+  "u": 0,
+  "v": 0,
+  "w": 0,
+  "x": 0,
+  "y": 0,
+  "z": 0
 }
 
-function countLetters(counter, sample_text){
+function countLetters(counter, sample_text) {
   // FIX ME
-}
+  //BASE CASE
+  if (sample_text.length === 0) {
+    return counter;
+  }
 
-$(document).ready(function(){
+  const key = sample_text[0].toLowerCase();
+
+  if (counter.hasOwnProperty(key)) {
+    counter[key]++;
+  }
+
+  countLetters(counter, sample_text.slice(1));
+};
+
+$(document).ready(function () {
   countLetters(counter, sample_text);
   $("#result").html(JSON.stringify(counter));
 });
